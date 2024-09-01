ac251c1fbf3963424e06e45ce62cc187bdb3581d
懋和道人 <15062733214@qq.com>
2024-09-01 23:18:25 +0800

    Update leancloud.js

--- utils/leancloud.js
+++ utils/leancloud.js
@@ -2,8 +2,8 @@
 import AV from 'leancloud-storage';
 
 // 从环境变量中读取 LeanCloud 配置
-const APP_ID = process.env.LEANCLOUD_APP_ID;
-const APP_KEY = process.env.LEANCLOUD_APP_KEY;
+const APP_ID = process.env.LEANCLOUD_APP_ID || process.env.leancloud-app-id;
+const APP_KEY = process.env.LEANCLOUD_APP_KEY || process.env.leancloud-app-key;
 
 AV.init({
     appId: APP_ID,
