6ef9d7056809eade6601c20a560cefec41d627a3
懋和道人 <15062733214@qq.com>
2024-09-01 23:56:38 +0800

    Update categories.js

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,19 +1,14 @@
 // /pages/api/categories.js
-import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-    const apiUrl = 'http://cdn.apc.360.cn/index.php?c=WallPaper&a=getAllCategoriesV2&from=360chrome';
+    const url = "http://cdn.apc.360.cn/index.php?c=WallPaper&a=getAllCategoriesV2&from=360chrome";
 
     try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
+        const response = await fetch(url);
+        const categories = await response.json();
 
-        if (!data || !data.data) {
-            return res.status(404).json({ error: '无法获取分类数据' });
-        }
-
-        res.status(200).json(data.data);
+        res.status(200).json(categories);
     } catch (error) {
-        res.status(500).json({ error: '获取分类失败' });
+        res.status(500).json({ error: 'Failed to fetch categories' });
     }
 }
